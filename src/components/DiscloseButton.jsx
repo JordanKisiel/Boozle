19b94f30f060c162e019f80a17214cf489cf7f97
Jordan Kisiel <jordan.kisiel@gmail.com>
2022-12-08 13:13:15 -0500

    Implemented opening and collapsing filters

--- src/components/DiscloseButton.jsx
+++ src/components/DiscloseButton.jsx
@@ -2,9 +2,23 @@ import React from 'react'
 
 export default function DiscloseButton(props){
 
+    let selectedArray = []
+
+    if(props.title !== 'Dislikes'){
+        selectedArray = props.ingredients.filter((ingredient) => {
+            return (props.filterState.ingredients.includes(ingredient))
+        })
+    }
+    else{
+        selectedArray = props.ingredients.filter((ingredient) => {
+            return (props.filterState.dislikes.includes(ingredient))
+        })
+    }  
+
     return (
-        <button className={`bg-blue-600 ${props.titleColor} text-xl w-full py-1 bg-[url('../assets/up-arrow.svg')] bg-no-repeat bg-[center_right_1rem] tracking-widest`}>
-            {props.title}
+        <button onClick={props.handleFilterOpen} className={`bg-blue-600 ${props.titleColor} text-xl w-full ${props.isOpen ? 'py-1' : 'py-3'} ${props.isOpen ? 'bg-[url(../assets/up-arrow.svg)]' : 'bg-[url(../assets/down-arrow.svg)]'} bg-no-repeat bg-[center_right_1rem] tracking-widest relative`}>
+            {!props.isOpen && <span className="absolute left-2 top-1/4 inline-block mt-0.5 bg-blue-900 rounded-full px-4 py-0.5 text-gray-600 italic text-sm tracking-widest">{`${selectedArray.length} selected`}</span>}
+            <span>{props.title}</span>
         </button>
     )
 }
--- src/components/FilterSelect.jsx
+++ src/components/FilterSelect.jsx
@@ -2,13 +2,21 @@ import React from 'react'
 import DiscloseButton from './DiscloseButton'
 import FilterButton from './FilterButton'
 import FilterSearch from './FilterSearch'
+import { motion, AnimatePresence } from "framer-motion"
 
 
 export default function FilterSelect(props){
     
-    const filterBtnArray = props.defaultIngredients.map((ingredient) => {
+    const [isOpen, setIsOpen] = React.useState(false)
+
+    function handleFilterOpen(){
+        setIsOpen((prevValue) => !prevValue)
+    }
+
+    const filterBtnArray = props.defaultIngredients.map((ingredient, index) => {
         return (
-            <FilterButton 
+            <FilterButton
+                key={index} 
                 name={ingredient} 
                 bgColor={props.bgColor}
                 filterCategory={props.title}
@@ -21,23 +29,39 @@ export default function FilterSelect(props){
     return (
         <>
             <div className="w-full rounded-sm overflow-hidden drop-shadow-xl md:w-3/4 lg:w-[48rem]">
-                <DiscloseButton title={props.title} titleColor={props.titleColor} />
-                <div className="bg-blue-300 px-5 py-5 space-y-8">
-                    <div className="flex flex-wrap gap-4">
-                        { filterBtnArray }
-                    </div>
-                    <div className="border-t-2 border-[rgba(255_255_255/0.08)] pt-4 pb-0 flex">
-                        <FilterSearch />
-                    </div>
-                </div>
-            </div>
-            
-            {/* Collapsed Filter */}
-            <div className="hidden w-full rounded-sm overflow-hidden drop-shadow-xl md:w-3/4 lg:w-[48rem]">
-                <button className="bg-blue-600 text-pink-300 text-xl w-full py-3 bg-[url('../assets/down-arrow.svg')] bg-no-repeat bg-[center_right_1rem] tracking-widest relative">
-                <span className="absolute left-2 top-1/4 inline-block mt-0.5 bg-blue-900 rounded-full px-4 py-0.5 text-gray-600 italic text-sm tracking-widest">2 selected</span>
-                <span>Mixer</span>
-                </button>
+                <DiscloseButton 
+                    title={props.title} titleColor={props.titleColor}
+                    isOpen={isOpen}
+                    ingredients={[...props.defaultIngredients, ...props.otherIngredients]}
+                    filterState={props.filterState}
+                    handleFilterOpen={handleFilterOpen} 
+                />
+                <AnimatePresence>
+                    {isOpen && (
+                        <motion.div
+                            initial="collapsed"
+                            animate="open"
+                            exit="collapsed"
+                            variants={{
+                                open: {height: "auto"},
+                                collapsed: {height: 0}
+                            }}
+                            transition={{
+                                duration: 0.7,
+                                ease: [0.04, 0.62, 0.23, 0.98]
+                            }} 
+                            className="bg-blue-300">
+                            <div className="px-5 py-5 space-y-8">
+                                <div className="flex flex-wrap gap-4">
+                                    { filterBtnArray }
+                                </div>
+                                <div className="border-t-2 border-[rgba(255_255_255/0.08)] pt-4 pb-0 flex">
+                                    <FilterSearch />
+                                </div>
+                            </div>
+                        </motion.div>
+                    )}
+                </AnimatePresence>
             </div>
         </>
     )
